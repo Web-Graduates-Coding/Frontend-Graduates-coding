594835bee0e932c3c4d72ab6b0844e3861cd1198
David Osorio <daalosx@gmail.com>
2024-09-19 16:23:53 -0500

    agregando bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CardComponent } from "./components/card/card.component";
+import { MessageCardComponent } from '@components/message-card/message-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, MessageCardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss', '../styles/variables.scss']
 })
---
+++ src/app/shared/components/message-card/message-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'message-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-card.component.html',
+  styleUrl: './message-card.component.scss'
+})
+export class MessageCardComponent {
+  
+}
