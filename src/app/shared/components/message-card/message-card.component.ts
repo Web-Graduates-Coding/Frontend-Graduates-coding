a49f13b5eeaff25bac759844388f864876b5c5c3
David Osorio <daalosx@gmail.com>
2024-09-19 18:27:32 -0500

    haciendo el responsive de la publicacion

--- src/app/shared/components/message-card/message-card.component.ts
+++ src/app/shared/components/message-card/message-card.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'message-card',
   standalone: true,
   imports: [],
   templateUrl: './message-card.component.html',
-  styleUrl: './message-card.component.scss'
+  styleUrls: ['./message-card.component.scss', '../../../../styles/variables.scss']
 })
 export class MessageCardComponent {
-  
+  @Input() public user: string = 'mensaje de prueba';
+  @Input() public tag: string = 'nombre de la cuenta';
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,6 @@ module.exports = {
     }
   },
   plugins: [],
-  darkMode: 'class',
+  darkMode: 'selector',
 }
 
